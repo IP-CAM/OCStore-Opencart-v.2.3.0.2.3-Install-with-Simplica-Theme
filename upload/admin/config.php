2ee175f54398d81377f4349858501c0d9f08086f
Hestia <San_San_01@mail.ru>
2019-05-30 17:54:22 +0300

    Initial commit

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocstore2.3.loc:8383/upload/admin/');
+define('HTTP_CATALOG', 'http://ocstore2.3.loc:8383/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocstore2.3.loc:8383/upload/admin/');
+define('HTTPS_CATALOG', 'http://ocstore2.3.loc:8383/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/admin/');
+define('DIR_SYSTEM', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/');
+define('DIR_IMAGE', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/image/');
+define('DIR_LANGUAGE', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/admin/language/');
+define('DIR_TEMPLATE', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/admin/view/template/');
+define('DIR_CONFIG', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/config/');
+define('DIR_CACHE', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/download/');
+define('DIR_LOGS', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/upload/');
+define('DIR_CATALOG', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opstore2.3db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocstore2.3.loc:8383/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocstore2.3.loc:8383/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/catalog/');
+define('DIR_SYSTEM', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/');
+define('DIR_IMAGE', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/image/');
+define('DIR_LANGUAGE', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/config/');
+define('DIR_CACHE', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/download/');
+define('DIR_LOGS', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Programs/OSPanel/domains/ocStore2.3.loc/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opstore2.3db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
